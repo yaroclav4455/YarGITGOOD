f23c095e87506928885fdecbc989ef4afff135bc
enoenot999@yandex.ru <enoenot999@yandex.ru>
2021-12-03 11:16:00 +0300

    u win

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ namespace YarGITGOOD
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Haw are u? ");
-            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("U Win!");
 
+            }
         }
     }
 }
