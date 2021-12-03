7e3d769dd970b09da0440fc09ff865709567cd65
enoenot999@yandex.ru <enoenot999@yandex.ru>
2021-12-03 11:21:41 +0300

    repit

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace YarGITGOOD
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Haw are u? ");
-            
-
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("U Win!");
+                Console.WriteLine("I Win!");
+                Console.WriteLine("we Win!");
+            }
         }
     }
 }
